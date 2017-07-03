2ff864391f1a9c88255590857267d392e634feec
John Samson <jsamson3@msudenver.edu>
2017-07-02 21:48:23 -0600

    Initial commit

---
+++ src/fantasyRealm.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Work_Time on 7/2/2017.
+ */
+public class fantasyRealm {
+
+    public static void main (String[] args) {
+        System.out.println("Hi how are you. ");
+    }
+}
