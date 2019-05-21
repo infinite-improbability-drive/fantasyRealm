60429776ca79243f231b44d071cd04155424c17a
DarkLORDCOSMOS <jsamson3@msudenver.edu>
2019-05-21 17:10:32 -0600

    Input player name, show as player icon, output Unicode characters (!)

--- C++/fantasyRealm/fantasyRealm/fantasyRealm.cpp
+++ C++/fantasyRealm/fantasyRealm/fantasyRealm.cpp
@@ -1,5 +1,5 @@
 /*
-OneLoneCoder.com - Command Line Snake
+OneLoneCoder.com - Command Line player
 "Give me a break, I'm on holiday..." - @Javidx9
 License
 ~~~~~~~
@@ -25,7 +25,7 @@ license here: https://github.com/OneLoneCoder/videos/blob/master/LICENSE
 Cheers!
 Background
 ~~~~~~~~~~
-Classic Snake! Controls are Arrow keys Left & Right, eat food, grow larger, avoid self!
+Classic player! Controls are Arrow keys Left & Right, eat food, grow larger, avoid self!
 Author
 ~~~~~~
 Twitter: @javidx9
@@ -41,54 +41,64 @@ Last Updated: 24/06/2017
 #include <list>
 #include <string>
 #include <thread>
+#include <fcntl.h>
+#include <io.h>
+#include <locale>
+#include <codecvt>
+#include <wchar.h>
+
 using namespace std;
 
 #include <Windows.h>
 
+
+char ch;
 int nScreenWidth = 120;
 int nScreenHeight = 30;
+string name;
+wstring wname;
+wchar_t* cname;
 
-struct sSnakeSegment
-{
+struct player {
 	int x;
 	int y;
 };
 
-int main()
-{
+wstring start();
+
+int main() {
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	// wprintf(L"\x263a\x263b");
+
+	wname = start();
+
 	// Create Screen Buffer
 	wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
 	for (int i = 0; i < nScreenWidth*nScreenHeight; i++) screen[i] = L' ';
 	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(hConsole);
 	DWORD dwBytesWritten = 0;
-
-	bool bKeyUp = false, bKeyDown = false, bKeyUpOld = false, bKeyDownOld = false;
-	bool bKeyLeft = false, bKeyRight = false, bKeyLeftOld = false, bKeyRightOld = false;
-
-	string name;
-	wsprintf(&screen[nScreenWidth + 5], L"Welcome to the fantasy realm.");
-
+	
 	while (1)
 	{
 
-		wsprintf(&screen[nScreenWidth + 5], L"Welcome to the fantasy realm.");
+		// wsprintf(&screen[nScreenWidth + 5], L"Welcome to the fantasy realm.");
 
 		// Reset to known state
-		list<sSnakeSegment> snake = { {60,15} };
+		player player = { 60,15 };
 		int nFoodX = 30;
 		int nFoodY = 15;
 		int nScore = 0;
-		int nSnakeDirection = 3;
+		int nplayerDirection = 3;
 		bool bDead = false;
-		// bool bKeyUp = false, bKeyDown = false, bKeyUpOld = false, bKeyDownOld = false;
-		// bool bKeyLeft = false, bKeyRight = false, bKeyLeftOld = false, bKeyRightOld = false;
+		bool bKeyUp = false, bKeyDown = false, bKeyUpOld = false, bKeyDownOld = false;
+		bool bKeyLeft = false, bKeyRight = false, bKeyLeftOld = false, bKeyRightOld = false;
 
 		while (!bDead)
 		{
 			// Frame Timing, compensate for aspect ratio of command line
 			auto t1 = chrono::system_clock::now();
-			while ((chrono::system_clock::now() - t1) < ((nSnakeDirection % 2 == 1) ? 120ms : 200ms))
+			while ((chrono::system_clock::now() - t1) < ((nplayerDirection % 2 == 1) ? 120ms : 200ms))
 			{
 				// Get Input, 
 				bKeyLeft = (0x8000 & GetAsyncKeyState((unsigned char)('\x25'))) != 0;
@@ -96,36 +106,10 @@ int main()
 				bKeyUp = (0x8000 & GetAsyncKeyState((unsigned char)('\x28'))) != 0;
 				bKeyDown = (0x8000 & GetAsyncKeyState((unsigned char)('\x26'))) != 0;
 
-				if (bKeyUp && !bKeyUpOld)
-				{
-					// nSnakeDirection = 1;
-					snake.push_front({ snake.front().x, snake.front().y + 1 });
-					// nSnakeDirection++;
-					// if (nSnakeDirection == 4) nSnakeDirection = 0;
-				}
-
-				if (bKeyDown && !bKeyDownOld)
-				{
-					// nSnakeDirection = 2;
-					snake.push_front({ snake.front().x, snake.front().y - 1 });
-					// nSnakeDirection--;
-					// if (nSnakeDirection == -1) nSnakeDirection = 3;
-				}
-				if (bKeyRight && !bKeyRightOld)
-				{
-					// nSnakeDirection = 1;
-					snake.push_front({ snake.front().x + 1, snake.front().y });
-					// nSnakeDirection++;
-					// if (nSnakeDirection == 4) nSnakeDirection = 0;
-				}
-
-				if (bKeyLeft && !bKeyLeftOld)
-				{
-					// nSnakeDirection = 3;
-					snake.push_front({ snake.front().x - 1, snake.front().y });
-					// nSnakeDirection--;
-					// if (nSnakeDirection == -1) nSnakeDirection = 3;
-				}
+				if (bKeyUp && !bKeyUpOld)		{ player.y = player.y + 1;}
+				if (bKeyDown && !bKeyDownOld)	{ player.y = player.y - 1;}
+				if (bKeyRight && !bKeyRightOld) { player.x = player.x + 1;}
+				if (bKeyLeft && !bKeyLeftOld)	{ player.x = player.x - 1;}
 
 				bKeyUpOld = bKeyUp;
 				bKeyDownOld = bKeyDown;
@@ -135,28 +119,8 @@ int main()
 
 			// ==== Logic
 
-			// Update Snake Position, place a new head at the front of the list in
-			// the right direction
-			/*
-			switch (nSnakeDirection)
-			{
-			case 0: // UP
-				snake.push_front({ snake.front().x, snake.front().y - 1 });
-				break;
-			case 1: // RIGHT
-				snake.push_front({ snake.front().x + 1, snake.front().y });
-				break;
-			case 2: // DOWN
-				snake.push_front({ snake.front().x, snake.front().y + 1 });
-				break;
-			case 3: // LEFT
-				snake.push_front({ snake.front().x - 1, snake.front().y });
-				break;
-			}
-			*/
-
-			// Collision Detect Snake V Food
-			if (snake.front().x == nFoodX && snake.front().y == nFoodY)
+			// Collision Detect player V Food
+			if (player.x == nFoodX && player.y == nFoodY)
 			{
 				nScore++;
 				while (screen[nFoodY * nScreenWidth + nFoodX] != L' ')
@@ -165,23 +129,15 @@ int main()
 					nFoodY = (rand() % (nScreenHeight - 3)) + 3;
 				}
 
-				for (int i = 0; i < 5; i++)
-					snake.push_back({ snake.back().x, snake.back().y });
+				// for (int i = 0; i < 5; i++)
+				// 	player.push_back({ player.back().x, player.back().y });
 			}
 
-			// Collision Detect Snake V World
-			if (snake.front().x < 0 || snake.front().x >= nScreenWidth)
+			// Collision Detect player V World
+			if (player.x < 0 || player.x >= nScreenWidth)
 				bDead = true;
-			if (snake.front().y < 3 || snake.front().y >= nScreenHeight)
+			if (player.y < 3 || player.y >= nScreenHeight)
 				bDead = true;
-
-			// Collision Detect Snake V Snake
-			// for (list<sSnakeSegment>::iterator i = snake.begin(); i != snake.end(); i++)
-			// 	if (i != snake.begin() && i->x == snake.front().x && i->y == snake.front().y)
-			// 		bDead = true;
-
-			// Chop off Snakes tail :-/
-			// snake.pop_back();
 
 			// ==== Presentation
 
@@ -194,18 +150,13 @@ int main()
 				screen[i] = L'=';
 				screen[2 * nScreenWidth + i] = L'=';
 			}
-			wsprintf(&screen[nScreenWidth + 5], L"Welcome to the fantasy realm.");
+			wsprintf(&screen[nScreenWidth + 5], L"%ls", wname.c_str());
 
+			// Draw player
+			screen[player.y * nScreenWidth + player.x] = bDead ? L'X' : ch;
 
-			// Draw Snake Body
-			for (auto s : snake)
-				screen[s.y * nScreenWidth + s.x] = bDead ? L'+' : L' ';
-
-			// Draw Snake Head
-			screen[snake.front().y * nScreenWidth + snake.front().x] = bDead ? L'X' : L'@';
-
-			// Draw Food
-			screen[nFoodY * nScreenWidth + nFoodX] = L'%';
+			// Draw food
+			screen[nFoodY * nScreenWidth + nFoodX] = L'\x2116';
 
 			if (bDead)
 				wsprintf(&screen[15 * nScreenWidth + 40], L"    PRESS 'SPACE' TO PLAY AGAIN    ");
@@ -219,5 +170,29 @@ int main()
 	}
 
 	return 0;
+}
+
+wstring start() {
+	wprintf(L"Welcome to the fantasy realm.\n");
+
+	wprintf(L"Please enter your name:\n");
+	getline(cin, name);
+
+	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+	// std::string narrow = converter.to_bytes(wide_utf16_source_string);
+	wstring wname = converter.from_bytes(name);
+	const wchar_t* cname = wname.c_str();
+
+	ch = wname[0];
+	wcout << "Hello " << wname << ".\n";
+	// wprintf(L"Hello %ls.\n", wname);
+
+	wprintf(L"Press enter to begin:\n");
+	string enter;
+	while ((0x8000 & GetAsyncKeyState((unsigned char)('\x12'))) != 0); {
+		getline(cin, enter);
+	}
+
+	return wname;
 }
 
